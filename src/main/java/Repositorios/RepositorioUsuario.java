1ceba5b35c269f284ab6bdf6896f1b92e21932e3
Carlos <cgonzalez@altimetrik.com>
2022-10-29 09:53:45 -0500

    Spring Boot Guia 3.3

---
+++ src/main/java/Repositorios/RepositorioUsuario.java
@@ -0,0 +1,7 @@
+package Repositorios;
+
+import Modelos.Usuario;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RepositorioUsuario extends MongoRepository<Usuario, String> {
+}
